e5bbffa8e0ad29d82a455c1403bcfc88754ec6cc
Rian_Lima_Silva <113356176+Rian-Lima-Silva@users.noreply.github.com>
2022-12-07 10:09:09 -0300

    Initial Commit

---
+++ ContadorRegressivo/ContadorRegressivo/FrmContador.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ContadorRegressivo
+{
+    partial class FrmContador
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtTempoFinal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.Contador = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTempoFinal
+            // 
+            this.txtTempoFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtTempoFinal.Location = new System.Drawing.Point(89, 21);
+            this.txtTempoFinal.Name = "txtTempoFinal";
+            this.txtTempoFinal.Size = new System.Drawing.Size(174, 26);
+            this.txtTempoFinal.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tempo:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F, System.Drawing.FontStyle.Bold);
+            this.lblTime.Location = new System.Drawing.Point(53, 77);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(325, 120);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "00:00";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.btnStart.Location = new System.Drawing.Point(289, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(115, 45);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::ContadorRegressivo.Properties.Resources.menu_02;
+            this.pictureBox.Location = new System.Drawing.Point(89, 63);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(247, 224);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // Contador
+            // 
+            this.Contador.Interval = 1000;
+            this.Contador.Tick += new System.EventHandler(this.Contador_Tick);
+            // 
+            // FrmContador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 305);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTempoFinal);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "FrmContador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contador Regressivo";
+            this.Load += new System.EventHandler(this.FrmContador_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTempoFinal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Timer Contador;
+    }
+}
+
---
+++ ContadorRegressivo/ContadorRegressivo/FrmContador.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContadorRegressivo
+{
+    public partial class FrmContador : Form
+    {
+        private int tempo = 0;
+        private int minuto = 0;
+        private int segundo = 0;
+
+        public FrmContador()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmContador_Load(object sender, EventArgs e)
+        {
+            pictureBox.Visible = false;
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //ContagemSegundos(5);
+            ContagemMinutos(5);
+            /**pictureBox.Visible = false;
+            lblTime.Visible = true;
+
+            //tempo = Convert.ToInt16(txtTempoFinal.Text);
+
+            if (tempo >= 60)
+            {
+                minuto = tempo / 60;
+                segundo = tempo % 60;//Resto da divisão
+            }
+            else
+            {
+                minuto = 0;
+                segundo = tempo;
+            }
+
+            lblTime.Text = "0" + minuto + ":" + segundo;
+
+            Contador.Enabled = true;//Disparar Timer*/
+        }
+
+        private void Contador_Tick(object sender, EventArgs e)
+        {
+            Timer_Evento();
+            /**segundo--;
+
+            if (minuto > 0)
+            {
+                if (segundo < 0)
+                {
+                    segundo = 59;
+                    minuto--;
+                }
+            }
+
+            lblTime.Text = "0" + minuto + ":" + segundo;
+
+            if (minuto == 0 && segundo == 0)
+            {
+                Contador.Enabled = false;
+                pictureBox.Visible = true;
+                lblTime.Visible = false;
+                txtTempoFinal.Clear();
+            }*/
+        }
+
+
+        private void ContagemSegundos(int seg)
+        {
+            if(seg>0)
+                tempo = seg;
+
+
+            pictureBox.Visible = false;
+            lblTime.Visible = true;
+
+            //tempo = Convert.ToInt16(txtTempoFinal.Text);
+
+            if (tempo >= 60)
+            {
+                minuto = tempo / 60;
+                segundo = tempo % 60;//Resto da divisão
+            }
+            else
+            {
+                minuto = 0;
+                segundo = tempo;
+            }
+
+            lblTime.Text = "0" + minuto + ":" + segundo;
+
+            Contador.Enabled = true;//Disparar Timer
+        }
+
+        private void ContagemMinutos(int min)
+        {
+            int temp=0;
+
+            if (min > 0)
+            {
+                temp=60;
+                minuto = min;
+            }
+                
+               
+
+            pictureBox.Visible = false;
+            lblTime.Visible = true;
+
+            if (minuto < 60)
+            {
+                segundo = temp % 60;//Resto da divisão
+            }
+            else if(minuto < 60 && minuto == 0)
+            {
+                segundo = minuto;
+            }
+            else
+            {
+                MessageBox.Show("Erro", "Contagem Minuto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            lblTime.Text = "0" + minuto + ":" + segundo;
+
+            //Contador.Interval = 1000;
+            Contador.Enabled = true;//Disparar Timer
+        }
+
+        private void Timer_Evento()
+        {
+            segundo--;
+
+            if (minuto > 0)
+            {
+                if (segundo < 0)
+                {
+                    segundo = 59;
+                    minuto--;
+                }
+            }
+
+            lblTime.Text = "0" + minuto + ":" + segundo;
+
+            if (minuto == 0 && segundo == 0)
+            {
+                Contador.Enabled = false;
+                pictureBox.Visible = true;
+                lblTime.Visible = false;
+            }
+
+        }
+
+
+    }//CLASSE
+}//NAMESPACE
