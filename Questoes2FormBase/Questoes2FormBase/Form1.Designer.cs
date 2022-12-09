c1f062606d45fcb88f0add850dc4083b142f067d
Rian_Lima_Silva <113356176+Rian-Lima-Silva@users.noreply.github.com>
2022-12-09 11:00:16 -0300

    Add files via upload

---
+++ Questoes2FormBase/Questoes2FormBase/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Questoes2FormBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.OpcaoA = new System.Windows.Forms.RadioButton();
+            this.OpcaoB = new System.Windows.Forms.RadioButton();
+            this.OpcaoC = new System.Windows.Forms.RadioButton();
+            this.OpcaoD = new System.Windows.Forms.RadioButton();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F);
+            this.label1.Location = new System.Drawing.Point(275, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pergunta";
+            // 
+            // OpcaoA
+            // 
+            this.OpcaoA.AutoSize = true;
+            this.OpcaoA.Enabled = false;
+            this.OpcaoA.Location = new System.Drawing.Point(308, 243);
+            this.OpcaoA.Name = "OpcaoA";
+            this.OpcaoA.Size = new System.Drawing.Size(85, 17);
+            this.OpcaoA.TabIndex = 1;
+            this.OpcaoA.Text = "radioButton1";
+            this.OpcaoA.UseVisualStyleBackColor = true;
+            // 
+            // OpcaoB
+            // 
+            this.OpcaoB.AutoSize = true;
+            this.OpcaoB.Location = new System.Drawing.Point(308, 266);
+            this.OpcaoB.Name = "OpcaoB";
+            this.OpcaoB.Size = new System.Drawing.Size(85, 17);
+            this.OpcaoB.TabIndex = 2;
+            this.OpcaoB.Text = "radioButton2";
+            this.OpcaoB.UseVisualStyleBackColor = true;
+            // 
+            // OpcaoC
+            // 
+            this.OpcaoC.AutoSize = true;
+            this.OpcaoC.Location = new System.Drawing.Point(308, 289);
+            this.OpcaoC.Name = "OpcaoC";
+            this.OpcaoC.Size = new System.Drawing.Size(85, 17);
+            this.OpcaoC.TabIndex = 3;
+            this.OpcaoC.Text = "radioButton3";
+            this.OpcaoC.UseVisualStyleBackColor = true;
+            // 
+            // OpcaoD
+            // 
+            this.OpcaoD.AutoSize = true;
+            this.OpcaoD.Location = new System.Drawing.Point(308, 312);
+            this.OpcaoD.Name = "OpcaoD";
+            this.OpcaoD.Size = new System.Drawing.Size(85, 17);
+            this.OpcaoD.TabIndex = 4;
+            this.OpcaoD.Text = "radioButton4";
+            this.OpcaoD.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(637, 342);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.OpcaoD);
+            this.Controls.Add(this.OpcaoC);
+            this.Controls.Add(this.OpcaoB);
+            this.Controls.Add(this.OpcaoA);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton OpcaoA;
+        private System.Windows.Forms.RadioButton OpcaoB;
+        private System.Windows.Forms.RadioButton OpcaoC;
+        private System.Windows.Forms.RadioButton OpcaoD;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ Questoes2FormBase/Questoes2FormBase/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Questoes2FormBase
+{
+    public partial class Form1 : Form
+    {
+        int questao = -1;
+        string[] perguntas=new string[]
+        {
+            "1º Pergunta",//0
+            "2º Pergunta",//1
+            "3º Pergunta",//2
+            "4º Pergunta",//3
+            "5º Pergunta",//4
+            "6º Pergunta",//5
+            "7º Pergunta",//6
+            "8º Pergunta",//7
+        };
+
+        string[] respostas = new string[7];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            BotaoEvento();
+            //if(OpcaoA.Enabled == false)
+            //{
+            //    questao++;
+            //    label1.Text = perguntas[questao];
+            //    OpcaoA.Enabled = true;//////////////
+            //    return;
+            //}
+            //if (ValidarDados())
+            //{
+            //    if (questao < 7)
+            //    {
+                    
+            //        label1.Text = perguntas[questao];
+            //        OpcaoA.Enabled = true;
+
+            //        if (OpcaoA.Checked == true && GabaritoA())//0-2
+            //        {
+            //            respostas[questao] = "Certo";
+            //        }
+            //        else if (OpcaoB.Checked == true && GabaritoA())//1-3
+            //        {
+            //            respostas[questao] = "Certo";
+            //        }
+            //        else if (OpcaoC.Checked == true && GabaritoA())//4-6
+            //        {
+            //            respostas[questao] = "Certo";
+            //        }
+            //        else if (OpcaoD.Checked == true && GabaritoA())//5-7
+            //        {
+            //            respostas[questao] = "Certo";
+            //        }
+            //        else
+            //        {
+            //            respostas[questao] = "Errada";
+            //        }
+
+            //        questao++;
+
+            //    }
+            //    else
+            //    {
+            //        label1.Text = ContagemRespostas(respostas);
+            //    }
+               
+            //}//ValidarDados
+            
+
+                
+       
+        }
+
+        private void BotaoEvento()
+        {
+            if (OpcaoA.Enabled == false)
+            {
+                questao++;
+                label1.Text = perguntas[questao];
+                OpcaoA.Enabled = true;//////////////
+                return;
+            }
+            if (ValidarDados())
+            {
+                if (questao < 7)
+                {
+
+                    label1.Text = perguntas[questao];
+                    OpcaoA.Enabled = true;
+
+                    if (OpcaoA.Checked == true && GabaritoMatematica())//0-2
+                    {
+                        respostas[questao] = "Certo";
+                    }
+                    else if (OpcaoB.Checked == true && GabaritoMatematica())//1-3
+                    {
+                        respostas[questao] = "Certo";
+                    }
+                    else if (OpcaoC.Checked == true && GabaritoMatematica())//4-6
+                    {
+                        respostas[questao] = "Certo";
+                    }
+                    else if (OpcaoD.Checked == true && GabaritoMatematica())//5-7
+                    {
+                        respostas[questao] = "Certo";
+                    }
+                    else
+                    {
+                        respostas[questao] = "Errada";
+                    }
+
+                    questao++;
+
+                }
+                else
+                {
+                    label1.Text = ContagemRespostas(respostas,8);
+                }
+
+            }//ValidarDados
+        }
+
+        private bool ValidarDados()
+        {
+            if (OpcaoA.Enabled == true && OpcaoA.Checked == false && OpcaoB.Checked == false && OpcaoC.Checked == false && OpcaoD.Checked == false)
+            {
+                MessageBox.Show("Selecione um resposta");
+                return false;
+            }
+           return true;
+        }
+
+        private bool GabaritoMatematica()
+        {
+
+            if(OpcaoA.Checked == true && questao == 0 || OpcaoA.Checked == true && questao == 2)//A
+            {
+               
+                return true;
+            }
+            else if (OpcaoB.Checked == true && questao == 1 || OpcaoB.Checked == true && questao == 3)//B
+            {
+                
+                return true;
+            }
+            else if(OpcaoC.Checked == true && questao == 4 || OpcaoC.Checked == true && questao == 6)//C
+            {
+                return true;
+            }
+            else if (OpcaoD.Checked == true && questao == 5 || OpcaoD.Checked == true && questao == 7)//D
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+        }
+        private bool Gabarito()
+        {
+
+            if(OpcaoA.Checked == true && questao == 0 || questao == 2)//A
+            {
+               
+                return true;
+            }
+            else if (OpcaoB.Checked == true && questao == 1 || questao == 3)//B
+            {
+                
+                return true;
+            }
+            else if(OpcaoC.Checked == true && questao == 4 || questao == 6)//C
+            {
+                return true;
+            }
+            else if (OpcaoD.Checked == true && questao == 5 || questao == 7)//D
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+        }
+
+        private string ContagemRespostas(string[] respostas,int numero_questao)
+        {
+            int RespostasCertas = (from resposta in respostas
+                       where resposta.ToString() == "Certo"
+                       select resposta).Count();
+
+            string resultado = "Resultado:" + RespostasCertas + "/"+numero_questao;
+
+            return resultado;
+
+
+        }
+    }
+}
